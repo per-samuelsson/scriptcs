6902ff3daa5786ef92705de6abbc9151a7a80837
Per Samuelsson <per@starcounter.com>
2014-01-14 10:03:30 +0100

    Added empty RoslynScriptExecutableEngine class, to hold some elaborate code for issue #23.

---
+++ src/ScriptCs.Engine.Roslyn/RoslynScriptExecutableEngine.cs
@@ -0,0 +1,30 @@
+﻿using Common.Logging;
+using Roslyn.Scripting;
+using ScriptCs.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.Engine.Roslyn {
+
+    /// <summary>
+    /// Temporary proof-of-concept engine, related to the implementation of
+    /// issue https://github.com/scriptcs/scriptcs/issues/23.
+    /// </summary>
+    public class RoslynScriptExecutableEngine : RoslynScriptEngine {
+
+        public RoslynScriptExecutableEngine(IScriptHostFactory scriptHostFactory, ILog logger)
+            : base(scriptHostFactory, logger) {
+        }
+
+        protected override ScriptResult Execute(string code, Session session) {
+            // TODO:
+            // Implement our thing
+
+            throw new NotImplementedException();
+        }
+    }
+}
