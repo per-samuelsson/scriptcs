a530fd1a9a224d210d92ccc318ea16b7c4d80519
Per Samuelsson <per@starcounter.com>
2014-01-14 12:42:06 +0100

    Made a showcase implementation of RoslynScriptExecutableEngine, to share the idea of what is possible to do with the compiled emission when we need to create an executable.

--- src/ScriptCs.Engine.Roslyn/RoslynScriptExecutableEngine.cs
+++ src/ScriptCs.Engine.Roslyn/RoslynScriptExecutableEngine.cs
@@ -1,12 +1,14 @@
 ﻿using Common.Logging;
+using Roslyn.Compilers;
 using Roslyn.Scripting;
+using Roslyn.Scripting.CSharp;
 using ScriptCs.Contracts;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Runtime.ExceptionServices;
 
 namespace ScriptCs.Engine.Roslyn {
 
@@ -15,16 +17,78 @@ namespace ScriptCs.Engine.Roslyn {
     /// issue https://github.com/scriptcs/scriptcs/issues/23.
     /// </summary>
     public class RoslynScriptExecutableEngine : RoslynScriptEngine {
+        const string RoslynAssemblyNameCharacter = "ℛ";
+        const string CompiledScriptClass = "Submission#0";
+        const string CompiledScriptMethod = "<Factory>";
 
         public RoslynScriptExecutableEngine(IScriptHostFactory scriptHostFactory, ILog logger)
             : base(scriptHostFactory, logger) {
         }
 
         protected override ScriptResult Execute(string code, Session session) {
-            // TODO:
-            // Implement our thing
+            Guard.AgainstNullArgument("session", session);
 
-            throw new NotImplementedException();
+            var result = new ScriptResult();
+            try {
+                var submission = session.CompileSubmission<object>(code);
+                var loaded = BuildExecutable(session, submission);
+                try {
+                    InvokeEntrypoint(session, loaded, result);
+                } catch (Exception ex) {
+                    result.ExecuteExceptionInfo = ExceptionDispatchInfo.Capture(ex);
+                }
+            } catch (Exception ex) {
+                result.CompileExceptionInfo = ExceptionDispatchInfo.Capture(ex);
+            }
+
+            return result;
+        }
+
+        private Assembly BuildExecutable(Session session, Submission<object> submission) {
+            var name = new AssemblyName(RoslynAssemblyNameCharacter + Guid.NewGuid().ToString());
+            var fileName = Path.ChangeExtension(FileName, ".exe");
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(name, AssemblyBuilderAccess.RunAndSave);
+            var scriptModule = assemblyBuilder.DefineDynamicModule(Path.GetFileNameWithoutExtension(fileName), fileName, true);
+
+            submission.Compilation.Emit(scriptModule);
+
+            var engineType = scriptModule.ResolveType(scriptModule.GetTypeToken(typeof(ScriptEngine)).Token);
+            var engineCtor = engineType.GetConstructor(new[] { typeof(MetadataFileProvider), typeof(IAssemblyLoader) });
+            var createSession = engineType.GetMethod("CreateSession", Type.EmptyTypes);
+            var sessionType = scriptModule.ResolveType(scriptModule.GetTypeToken(typeof(Session)).Token);
+            var factory = scriptModule.GetType(CompiledScriptClass).GetMethod(CompiledScriptMethod, BindingFlags.Static | BindingFlags.Public);
+
+            var entrypoint = scriptModule.DefineGlobalMethod("Main", MethodAttributes.Private | MethodAttributes.Static | MethodAttributes.HideBySig, typeof(void), Type.EmptyTypes);
+            var il = entrypoint.GetILGenerator();
+            il.DeclareLocal(sessionType);
+            il.Emit(OpCodes.Ldnull);
+            il.Emit(OpCodes.Ldnull);
+            il.Emit(OpCodes.Newobj, engineCtor);
+            il.Emit(OpCodes.Call, createSession);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Call, factory);
+            il.Emit(OpCodes.Pop);
+            il.Emit(OpCodes.Ret);
+
+            scriptModule.CreateGlobalFunctions();
+            assemblyBuilder.SetEntryPoint(entrypoint.GetBaseDefinition());
+
+            assemblyBuilder.Save(fileName);
+
+            return assemblyBuilder;
+        }
+
+        private void InvokeEntrypoint(Session session, Assembly assembly, ScriptResult scriptResult) {
+            var type = assembly.GetType(CompiledScriptClass);
+            var method = type.GetMethod(CompiledScriptMethod, BindingFlags.Static | BindingFlags.Public);
+
+            try {
+                scriptResult.ReturnValue = method.Invoke(null, new[] { session });
+            } catch (Exception executeException) {
+                var ex = executeException.InnerException ?? executeException;
+                scriptResult.ExecuteExceptionInfo = ExceptionDispatchInfo.Capture(ex);
+            }
         }
     }
 }
